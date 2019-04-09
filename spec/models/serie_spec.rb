fde2a7fcf9e1ecd1e951adc0cf003a56bc1e6cc3
Valeria Vassallo <valevassallo1@gmail.com>
2019-04-09 09:12:41 -0500

    Comment pendings

--- spec/models/serie_spec.rb
+++ spec/models/serie_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe Serie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# RSpec.describe Serie, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
