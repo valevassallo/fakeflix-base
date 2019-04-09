0d204ec6624f0fe075c87c8b0604972ed0ab0d3e
Valeria Vassallo <valevassallo1@gmail.com>
2019-04-09 09:07:35 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    resources :movies, only: [:index, :show, :update] do
+      patch 'playback', on: :member
+      patch 'rating', on: :member
+    end
+    resources :series, only: [:index, :show, :update] do
+      patch 'rating', on: :member
+    end
+    resources :episodes, only: [:show, :update] do
+      patch 'playback', on: :member
+    end
+    resources :rents, controller: 'rentals', only: [:index] do
+      post '/movies/:id' => :movies, on: :collection
+      post '/series/:id' => :series, on: :collection
+    end
+  end
 end
